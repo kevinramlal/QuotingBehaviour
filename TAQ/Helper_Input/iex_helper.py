1ca652ea2bbd1cf31a5c05782502cbd063699990
Kevin Ramlal <kevinramlal58@gmail.com>
2019-09-21 19:59:39 -0700

    Better NBB Tracker

--- TAQ/Helper_Input/iex_helper.py
+++ TAQ/Helper_Input/iex_helper.py
@@ -47,25 +47,78 @@ class quote_wrangler:
 		self.quotes_cols = list_from_csv(self.my_dir + '.\quotes_columns.csv')
 		self.quotes_df = self.quotes_df[self.quotes_cols]
 
-	def BBO_series(self, use_two = False):
+	def BBO_series(self):
 		"""
 		Returns a dataframe that contains ONLY BBOs
 		"""
 		#FIRST filter only qu_conditions for O ,R, or Y
 		temp = self.quotes_df[(self.quotes_df['QU_COND'] == 'O') | (self.quotes_df['QU_COND'] == 'R') | (self.quotes_df['QU_COND'] == 'Y')]
-		if use_two:
-			return temp[(temp['NATBBO_IND'] ==4) | (temp['NATBBO_IND'] ==2)]
-		else:
-			return temp[(temp['NATBBO_IND'] ==4)]
-			
+		#I dont think using 2 and 4 is right
+		# if use_two:
+		# 	return temp[(temp['NATBBO_IND'] ==4) | (temp['NATBBO_IND'] ==2)]
+		# else:
+		# 	return temp[(temp['NATBBO_IND'] ==4)]
+		return temp 
+	
+
+	def NBB_combiner(self):
+		filtered_df = self.BBO_series()
+		#[bid,bid_size,ask,ask_size]
+		ex_bid_price = {k:0 for k in self.exchange_map.keys()}
+		ex_bid_size = ex_bid_price.copy()
+		ex_ask_price = ex_bid_price.copy()
+		ex_ask_size = ex_bid_price.copy()
+		master = []
+		# cols = ['BID','BIDSIZ','ASK','ASKSIZ']
+		cols = ['BID']
+		prev_best_bid = 0
+		for msg in range(len(filtered_df)):
+			#update dictionaries
+			ex_bid_price[filtered_df['EX'].iloc[msg]] = float(filtered_df['BID'].iloc[msg]) #update dict
+			ex_bid_size[filtered_df['EX'].iloc[msg]] = float(filtered_df['BIDSIZ'].iloc[msg]) #update dict
+			ex_ask_price[filtered_df['EX'].iloc[msg]] = float(filtered_df['ASK'].iloc[msg]) #update dict
+			ex_ask_size[filtered_df['EX'].iloc[msg]] = float(filtered_df['ASKSIZ'].iloc[msg]) #update dict
+			itemMaxValue = max(ex_bid_price.items(), key=lambda x: x[1]) #find new max 
+			ex_at_nbb = list()
+			# Iterate over all the items in dictionary to find keys with max value
+			for key, value in ex_bid_price.items():
+				if value == itemMaxValue[1]:
+					ex_at_nbb.append(key) #there should always be one max?
+			if itemMaxValue[1] != prev_best_bid:
+				best_bid = itemMaxValue[1]
+				bid_vol = sum(ex_bid_size[ex] for ex in ex_at_nbb)
+				best_ask = ex_ask_price[ex_at_nbb[0]] #not sure 
+				ask_vol = sum(ex_ask_size[ex] for ex in ex_at_nbb) #notsure 
+				exchanges = ex_at_nbb
+				time = filtered_df['Time'].iloc[msg]
+				master.append([time,exchanges,best_bid,bid_vol,best_ask,ask_vol])
+				prev_best_bid = best_bid
+		master_df = pd.DataFrame(master)
+		master_df.columns = ['Time','Exchanges','NBB','Bid Size Total', 'Best Ask', 'Ask Vol']
+		return master_df
+
+
+	def BBO_tracker(self,EX):
+		"""
+		Want to keep track of Best bid, exchange, size (both by exchange and total), time of update, (START WITH JUST BID)
+		"""
+		filtered_df = self.BBO_series(use_two =True)
+		return filtered_df[[filtered_df['EX'] == EX]]		
+		# cols = ['DateTime','Time','EX','BID','BIDSIZ','ASK','ASKSIZ']
+		# tracker_template = np.array(filtered_df[cols][0])
+		# for q in range(1,len(filtered_df)):
+		# 	if filtered_df['BID'][q] == tracker_template[q-1][3]:
+		# 		exchanges_at_NBB = np.array(filtered_df[q-1])
+		# return q
 
 
 def main():
 	# print(dict_create('./exchange_code_dict.csv'))
 	# print(list_from_csv('./quotes_columns.csv'))
-	quotes_file = '../AAPL.1.5.18.csv'
-	trades_file = '../Trades1.5.18.csv'
-	return stock_data(quotes_file,trades_file)
+	quotes_file = '../AAPL.1.4.18_training.csv'
+	q = quote_wrangler(quotes_file)
+	q2 = q.exchange_combiner()
+	return q2
 
 if __name__== '__main__':
-	main()
+	q = main()
